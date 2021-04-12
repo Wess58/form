dcfb33830d8000881d07608ed9aec3589ccc7279
wesley mbate <wmbate@meliora.tech>
2021-04-13 00:02:06 +0300

    created form and page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form';
+  hide: Boolean = true;
+  nameInvalid: Boolean = false;
+  emailInvalid: Boolean = false;
+  optionInvalid: Boolean = false;
+  passwordInvalid: Boolean = false;
+
+  year:any = new Date().getFullYear();
+
+
+  person: any = {
+    name: '',
+    email: '',
+    option: '',
+    password: ''
+  }
+
+
+  validateForm(): void {
+
+    // name
+    if (!this.person.name) {
+      this.nameInvalid = true;
+    } else {
+      this.nameInvalid = false;
+    }
+
+    // email
+    if (/\S+@\S+\.\S+/.test(this.person.email)) {
+      this.emailInvalid = false;
+    } else {
+      this.emailInvalid = true;
+    }
+
+    // option
+    if (!this.person.option) {
+      this.optionInvalid = true;
+    } else {
+      this.optionInvalid = false;
+    }
+
+    // password
+    if (this.person.password.length >= 6) {
+      this.passwordInvalid = false;
+    } else {
+      this.passwordInvalid = true;
+    }
+
+  }
+
 }
