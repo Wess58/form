3737b78afdd2321dc4d5f5a149dd222bfc41d39f
wesley mbate <wmbate@meliora.tech>
2021-04-13 00:00:07 +0300

    import for material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material/material.module';
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +16,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
